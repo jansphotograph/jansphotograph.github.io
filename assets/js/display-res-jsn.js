8d7c1c5783cde34039a77f20653b8e601271ce33
Jans_Photograph <jans.pty@gmail.com>
2024-08-06 23:08:40 -0500

    Delete assets/js/display-res-jsn.js

--- assets/js/display-res-jsn.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.getElementById("#menu-resp");
-const navBar = document.querySelector('.nav-bar');
-
-menuButton.addEventListener('onClick', () => {
-    navBar.classList.toggle('visible'); // O 'active', 'visible', etc. según tu CSS
-});
